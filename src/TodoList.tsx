1c51c6cf44c04ef367ea142e445774d0cb92a4ad
MaxTynkovich <m_wildfires@mail.ru>
2021-05-01 23:53:53 +0300

    refactoring

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -10,7 +10,8 @@ type TodoListPropsType = {
 
 function TodoList(props: TodoListPropsType) {
     const tasks = props.tasks.map(t => {
-        return <li key={t.id}><input type="checkbox" checked={t.isDone} /> <span>{t.title}</span><button onClick={()=>props.removeTask(t.id)}>X</button></li>
+            const removeTask = () => props.removeTask(t.id)
+        return <li key={t.id}><input type="checkbox" checked={t.isDone} /> <span>{t.title}</span><button onClick={removeTask}>X</button></li>
     })
     const setAllFilterValue = () => props.changeTodoListFilter("all")
     const setActiveFilterValue = () => props.changeTodoListFilter("active")
