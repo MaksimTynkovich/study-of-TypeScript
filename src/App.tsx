d541c36693bc2bd9f27d9b8d0d9b4321e068b16f
MaxTynkovich <m_wildfires@mail.ru>
2021-05-01 21:45:44 +0300

    Adding and typification props for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 import TodoList from './TodoList'
 
+export type TaskType = {
+    title: string
+    id: number
+    isDone: boolean
+}
+
 function App() {
+    const tasks: Array<TaskType> = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "React", isDone: false },
+    ]
     return (
         <div className="App">
-           <TodoList title={"What to learn"}/>
+            <TodoList title={"What to learn"} tasks={tasks} />
         </div>
     );
 }
