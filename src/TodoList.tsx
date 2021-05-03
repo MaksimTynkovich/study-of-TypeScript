57bb865246975fc3754651a7accb6886fef964f2
MaxTynkovich <m_wildfires@mail.ru>
2021-05-03 23:25:07 +0300

    Added function to limit empty lines

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from 'react';
-import {FilterValuesType, TaskType} from './App'
+import { FilterValuesType, TaskType } from './App'
 
 type TodoListPropsType = {
     title: string
@@ -12,40 +12,54 @@ type TodoListPropsType = {
 
 function TodoList(props: TodoListPropsType) {
     const tasks = props.tasks.map(t => {
-            const removeTask = () => props.removeTask(t.id)
-            const changeStatus = (e: ChangeEvent<HTMLInputElement>) => props.changeTaskStatus(t.id, e.currentTarget.checked)
+        const removeTask = () => props.removeTask(t.id)
+        const changeStatus = (e: ChangeEvent<HTMLInputElement>) => props.changeTaskStatus(t.id, e.currentTarget.checked)
         return (
-        <li key={t.id}>
-            <input type="checkbox" 
-            checked={t.isDone} 
-            onChange={changeStatus} 
-            /> 
-            
+            <li key={t.id}>
+                <input type="checkbox"
+                    checked={t.isDone}
+                    onChange={changeStatus}
+                />
 
-            <span>{t.title}</span>
-            <button onClick={removeTask}>X</button>
-        </li>
-        )  })
+
+                <span>{t.title}</span>
+                <button onClick={removeTask}>X</button>
+            </li>
+        )
+    })
     const setAllFilterValue = () => props.changeTodoListFilter("all")
     const setActiveFilterValue = () => props.changeTodoListFilter("active")
     const setCompletedFilterValue = () => props.changeTodoListFilter("completed")
-    const addTask = () => { 
-        props.addTask(title)
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setError(null)
+        setTitle(e.currentTarget.value)
+    }
+    const addTask = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            props.addTask(trimmedTitle)
+        } else {
+            setError('Title is required!')
+        }
         setTitle("")
-    } 
+    }
     let [title, setTitle] = useState<string>('')
+    const [error, setError] = useState<string | null>(null)
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input value={title}
-                onChange={(e) =>{setTitle(e.currentTarget.value)}}
-                onKeyPress={(e) => { if (e.charCode === 13){addTask()}}}/>
+                <input 
+                    value={title}
+                    className={error ? 'error' : ''}
+                    onChange={changeTitle}
+                    onKeyPress={(e) => { if (e.charCode === 13) { addTask() } }} />
                 <button
-                 onClick={addTask}>+</button>
+                    onClick={addTask}>+</button>
+                    {error && <div className="error-text">{error}</div>}
             </div>
             <ul>
-               {tasks}
+                {tasks}
             </ul>
             <div>
                 <button onClick={setAllFilterValue}>All</button>
