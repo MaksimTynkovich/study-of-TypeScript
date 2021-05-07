b79faf2465eb3aacd22b5a0fece0e57ce0941809
MaxTynkovich <m_wildfires@mail.ru>
2021-05-07 21:48:02 +0300

    Type creation AddTodoListAT & RemoveTodoListAT, creation a switch in todolistsReducer

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,5 +1,26 @@
 import { TodoListType } from "../App";
 
-export const todolistsReducer = (todoLists: Array<TodoListType>) => {
+type RemoveTodoListAT = {
+    type: 'REMOVE-TODOLIST'
+    todoListID: string
+}
 
+type AddTodoListAT = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
+
+export const todolistsReducer = (todoLists: Array<TodoListType>, action: RemoveTodoListAT | AddTodoListAT) => {
+    switch(action.type) {
+        case 'REMOVE-TODOLIST':
+            return todoLists.filter(tl => tl.id !== action.todoListID)
+        case: 'ADD-TODOLIST':
+        const newtodoListID = v1()
+        const newTodoList: TodoListType = {
+            id: newtodoListID, title: title, filter: "all"
+        }
+            return [...todoLists, newTodoList]
+            default: 
+            return todoLists
+    }
 }
